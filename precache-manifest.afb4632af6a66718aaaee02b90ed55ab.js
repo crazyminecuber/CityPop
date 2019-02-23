e3a8e558ddd60d06fd9a3cbc0539098e9e235c96
Oskar <crazyminecuber@gmail.com>
2019-02-23 17:50:22 +0100

    Updates

---
+++ precache-manifest.afb4632af6a66718aaaee02b90ed55ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a2f745a3a656175696",
+    "url": "/CityPop/static/js/runtime~main.83a2f745.js"
+  },
+  {
+    "revision": "8bceab465cfd2611f59e",
+    "url": "/CityPop/static/js/main.8bceab46.chunk.js"
+  },
+  {
+    "revision": "c24ac6f76a092cb6e8de",
+    "url": "/CityPop/static/js/2.c24ac6f7.chunk.js"
+  },
+  {
+    "revision": "8bceab465cfd2611f59e",
+    "url": "/CityPop/static/css/main.5b591d4b.chunk.css"
+  },
+  {
+    "revision": "79a2544eb3a48724fd6f7fc750618c19",
+    "url": "/CityPop/index.html"
+  }
+];
